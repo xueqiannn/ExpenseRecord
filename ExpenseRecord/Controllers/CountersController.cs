bd51ef666d1d0a57cf77b30463a91294b358f757
Xueqian Li <XLi112@slb.com>
2023-10-27 13:37:33 +0800

    modify style

---
+++ ExpenseRecord/Controllers/CountersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseRecord.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountersController : ControllerBase
+    {
+        [HttpGet]
+        public List<int> get()
+        {
+            return new List<int>();
+        }
+    }
+}
